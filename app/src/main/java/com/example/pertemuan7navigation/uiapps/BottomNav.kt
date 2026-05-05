8e7cc79a37c6706d718d313c1cfe7a7514738f42
Rozzi21 <fakhruddinarrozi@gmail.com>
2026-05-05 13:58:54 +0700

    add Bottom nav

--- app/src/main/java/com/example/pertemuan7navigation/uiapps/BottomNav.kt
+++ app/src/main/java/com/example/pertemuan7navigation/uiapps/BottomNav.kt
@@ -1,4 +1,49 @@
 package com.example.pertemuan7navigation.uiapps
 
-class BottomNav {
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.pertemuan7navigation.navigation.Destination
+
+@Composable
+fun BottomNav(
+    navController: NavController,
+    items: List<Destination>
+) {
+    NavigationBar{
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        items.forEach {item ->
+            NavigationBarItem(
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+
+                        restoreState = true
+                    }
+                },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = item.icon),
+                        contentDescription = item.title
+                    )
+                },
+                label = {
+                    Text(text = item.title)
+                }
+            )
+        }
+    }
 }
